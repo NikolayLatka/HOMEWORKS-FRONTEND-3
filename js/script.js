a58e982f93b14046b8816e7fa43de150e0dfbbda
NikolayLatka <latkaa4@gmail.com>
2024-12-01 18:17:10 +0200

    add-js

---
+++ js/script.js
@@ -0,0 +1,63 @@
+// age
+
+// const age = 15;
+
+// console.log(age);
+
+// name
+
+// const name = "Mykola";
+
+// console.log(name);
+
+// isStudent
+
+// const isStudent = true;
+
+// console.log(isStudent);
+
+// myString
+
+// const myString =
+//   "You forget a thousand things every day, make sure this is one of them";
+
+// console.log(myString);
+
+// myNumber
+
+// let myNumber = 52;
+
+// myNumber = myNumber + 10;
+
+// console.log(myNumber);
+
+// myNull
+
+// const myNull = null;
+
+// console.log(myNull);
+
+// userName
+
+// const userName = prompt("Type your name");
+
+// alert(userName);
+
+// console.log(typeof userName);
+// string;
+
+// userAnswer
+
+// const userAnswer = confirm("Do you relly want to leave this page?");
+
+// console.log(userAnswer);
+
+// warning
+
+// const warning = "Warning! This action may be dangerous.";
+
+// alert(warning);
+
+// const sure = confirm("Are you sure that you want to continue?");
+
+// console.log(sure);
