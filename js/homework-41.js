786fd0796903b8f06aadba0fa57ea450bd4d2cc0
NikolayLatka <latkaa4@gmail.com>
2024-12-15 23:36:26 +0200

    add-hw-41

---
+++ js/homework-41.js
@@ -0,0 +1,122 @@
+// task 1
+
+// const celsius = 30;
+
+// const fahrenheit = (9 / 5) * celsius + 32;
+
+// console.log(fahrenheit);
+
+// // task 2
+
+// const daysInMonth = 31;
+
+// const hoursInMonth = daysInMonth * 24;
+
+// const minutesInMonth = 60 * hoursInMonth;
+
+// console.log(minutesInMonth);
+
+// // task 3
+
+// // Створити змінні health, energy для зберігання рівня здоров'я та енергії гравця в грі.
+// // Зменшити рівень здоров'я та енергії гравця
+// // за допомогою операторів віднімання
+// // та вивести результат в консоль
+
+// let health = 100;
+
+// let stamina = 100;
+
+// health = health - 10;
+
+// stamina -= 10;
+
+// console.log(health, stamina);
+
+// // task 4
+
+// // 4. Створити змінну totalPrice для зберігання суми покупки в магазині.
+// // Застосувати знижку discount в розмірі 10 % до цієї суми за допомогою оператора множення.
+// // Результат зберегти в змінній discountedPrice та вивести результат в консоль.
+
+// const totalPrice = 1200;
+
+// const discount = 0.1;
+
+// const discountedPrice = totalPrice * discount;
+
+// console.log(discountedPrice);
+
+// // task 5
+
+// // Створити змінну const floatNumber = 12.78 для зберігання числа з плаваючою комою.
+// // Використати метод Math.floor() для округлення числа до меншого.
+// // Результат зберегти в змінній roundedDown та вивести результат в консоль.
+
+// const floatNumber = 12.78;
+
+// const roundedDown = Math.floor(floatNumber);
+
+// console.log(roundedDown);
+
+// // task 6
+
+// // Створити змінну const floatString = "45.67"  для зберігання рядка, який містить числа з плаваючою комою.
+// // Використати метод parseFloat() для перетворення рядка у десяткове число.
+// // Результат зберегти в змінній parsedFloat та вивести результат в консоль.
+
+// const floatString = "45.67gjhk";
+
+// const parsedFloat = parseFloat(floatString);
+
+// console.log(parsedFloat);
+
+// task 7
+
+// Створити змінну const intString = "123" для зберігання рядка, який містить ціле число.
+// Використати метод parseInt() для перетворення рядка у ціле число.
+// Результат зберегти в змінній parsedIntта вивести результат в консоль.
+
+// const intString = "123";
+
+// const parsedInt = parseInt(intString);
+
+// console.log(parsedInt);
+
+// task 8
+
+// Створити змінну number для зберігання числа.
+// Використати метод Math.sqrt() для обчислення квадратного кореня числа.
+// Результат зберегти в змінній sqrtNumber та вивести результат в консоль.
+
+// const number = 16;
+
+// const sqrtNumber = Math.sqrt(number);
+
+// console.log(sqrtNumber);
+
+// task 9
+
+// За допомогою об’єкту Math виведіть в консоль
+// найбільше і найменше значення серед заданих
+// (2, 34, 99, 3, 22, 36, 733, 18)
+
+// const theBiggestNumbers = Math.max(2, 34, 99, 3, 22, 36, 733, 18);
+
+// const theSmallestNumbers = Math.min(2, 34, 99, 3, 22, 36, 733, 18);
+
+// console.log(theBiggestNumbers);
+
+// console.log(theSmallestNumbers);
+
+// extra task
+
+// У змінну random згенеруйте випадкове число від 3 до 19 включно.
+// Результат виведіть в консоль
+
+// Example
+// Math.random() * (max - min) + min;
+
+// const random = Math.random() * (1000000 - 10) + 10;
+
+// console.log(random);
