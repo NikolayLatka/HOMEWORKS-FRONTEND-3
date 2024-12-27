c4d7e760a4daccb99af73c974272ddbcc0df77b8
NikolayLatka <latkaa4@gmail.com>
2024-12-28 01:29:13 +0200

    add-hw-42

---
+++ js/homework-42.js
@@ -0,0 +1,57 @@
+// 1. Вивести result в консоль. Перевірити тип змінної result= 5+5+’5’.
+
+// const result = 5 + 5 + "5";
+
+// console.log(result, typeof result);
+
+// 2. Створіть змінну email з вашою електронною адресою.
+// Напишіть скрипт, який перевіряє чи містить змінна email символ @ (за доп метода includes()  або indexOf()) та рахує загальну кількість символів.
+// Результат виведіть в консоль.
+
+// const email = "example@gmail.com";
+
+// console.log(email.includes("@"), email.length);
+
+// 3. Записати в окремих змінних кожне слово: My, name, is.
+// Поєднати всі слова в змінну fullName.
+// До змінної fullName додати ‘Viktor’. fullName вивести в консоль.
+
+// const partMy = "My";
+
+// const partName = "name";
+
+// const partIs = "is";
+
+// const fullName = `${partMy} ${partName} ${partIs} Viktor`;
+
+// console.log(fullName);
+
+// 4. Створіть змінну userName з ім’ям користувача і змінну payment з сумою до оплати.
+// За допомогою шаблонного рядка виведіть на екран через alert повідомлення на прикладі цього:
+// «Дякуємо, Олександро! До сплати 300 гривень»
+
+// const userName = "Chicka Chicka Slim Shady";
+
+// const payment = 64;
+
+// alert(`Thank you, ${userName}! You must to pay us ${payment} freaking bucks`);
+
+// Додаткові для практики:
+
+// 5. Отримати  останній символ :
+// const fruit = 'апельсин';
+
+// const fruit = "апельсин";
+
+// console.log(fruit[fruit.length - 1]);
+
+// 6. знайди індекс фрази "neiS" в рядку  "Jason Neis"
+// Здійсни перевірку незалежно від регістру
+// const str = "Jason Neis"
+// const searchWord = "neiS"
+
+// const str = "Jason Neis";
+
+// const searchWord = "neiS";
+
+// console.log(str.toLowerCase().indexOf(searchWord.toLowerCase()));
