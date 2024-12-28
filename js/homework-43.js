71d24ae0f69251245313844553d0c6ae4538009f
NikolayLatka <latkaa4@gmail.com>
2024-12-28 21:02:48 +0200

    add-hw-43

---
+++ js/homework-43.js
@@ -0,0 +1,79 @@
+// 1. Створіть дві змінні для зберігання значень двох текстових полів.
+// Перевірте, чи обидві змінні не є порожніми. Якщо обидва поля заповнені,
+// вивести повідомлення "Обидва поля заповнені", якщо ні — "Не всі поля заповнені".
+
+// const text1 = "text1";
+
+// const text2 = "text2";
+
+// if (text1.trim() !== "" && text2.trim() !== "") {
+//   console.log("Both texts are here");
+// } else {
+//   console.log("Not all texts are here");
+// }
+
+// 2. Створіть дві змінні для зберігання числових значень.
+// Обчисліть їх суму. Якщо сума більше 10 — виведіть "Сума більша за 10",
+// якщо ні — "Сума менша або дорівнює 10".
+
+// const num1 = 8;
+
+// const num2 = 3;
+
+// const sum = num1 + num2;
+
+// if (sum > 10) {
+//   console.log("Sum is more than 10");
+// } else {
+//   console.log("Sum is less than 10 or it is 10");
+// }
+
+// 3. Створіть змінну для зберігання тексту.
+// Перевірте, чи містить цей текст слово "JavaScript".
+// Якщо містить, виведіть "Текст містить слово JavaScript",
+// якщо ні — "Текст не містить слово JavaScript".
+
+// const text = "Python";
+
+// if (text.includes("JavaScript")) {
+//   console.log("Text includes word: Javascript");
+// } else {
+//   console.log("Text doesn't include word: Javascript");
+// }
+
+// 4. Створіть змінну для зберігання числа.
+// Перевірте, чи це число більше 10 і менше 20.
+// Якщо так — виведіть "Число входить в діапазон від 10 до 20",
+// якщо ні — "Число не входить в діапазон від 10 до 20".
+
+// const num = 17;
+
+// if (num > 10 && num < 20) {
+//   console.log("Num is in diapozoooooon");
+// } else {
+//   console.log("Num is out of the diapozoooon");
+// }
+
+// 5. Створіть змінні для зберігання значень полів (ім'я, email, пароль).
+// Перевірте, чи ім'я містить не менше 3 символів,
+// чи email містить символ @ та крапку після нього,
+// а пароль не менше 6 символів.
+// Якщо всі умови виконані — виведіть "Перенаправлення на іншу сторінку",
+// якщо ні — "Помилка: неправильне заповнення".
+
+// const name = "Eazy-E";
+
+// const email = "Eazy-E@gmail.com";
+
+// const code = "64 Impala";
+
+// if (
+//   name.length >= 3 &&
+//   email.includes("@") &&
+//   email.lastIndexOf(".") > email.indexOf("@") &&
+//   code.length >= 6
+// ) {
+//   console.log("Teleport to next page");
+// } else {
+//   console.log("Error: not correct text");
+// }
