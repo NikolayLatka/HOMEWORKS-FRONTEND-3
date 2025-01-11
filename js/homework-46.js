f4dafa6fc47516c28b33d7c12628bef745ed03d3
NikolayLatka <latkaa4@gmail.com>
2025-01-11 20:09:07 +0200

    add-hw-46

---
+++ js/homework-46.js
@@ -0,0 +1,86 @@
+// Створити масив з трьох чисел.
+// Змінити значення другого елемента масиву на 10.
+
+// const numbers = [0, 1, 2];
+
+// numbers[1] = 10;
+
+// console.log(numbers);
+
+// Створити масив із трьох рядків.
+// Додати до масиву ще одну рядків.
+
+// const favAlbums = ["Eazy-Duz-It", "CHROMAKOPIA", "GNX"];
+
+// favAlbums[3] = "Utopia";
+
+// console.log(`Here are few of my fav albums: ${favAlbums}`);
+
+// Створити скрипт який поверне суму всіх чисел в масиві.
+
+// const numbers = [0, 1, 2];
+
+// let total = 0;
+
+// for (let i = 0; i < numbers.length; i += 1) {
+//   total += numbers[i];
+// }
+
+// console.log(total);
+
+// Створити масив з 5-ти чисел.
+// Вивести на екран всі елементи масиву за допомогою циклу for.
+
+// const numbers = [0, 1, 2, 3, 4];
+
+// for (let i = 0; i <= numbers.length - 1; i += 1) {
+//   console.log(numbers[i]);
+// }
+
+// Створити масив із 5-ти рядків.
+// Вивести на екран кожен рядокз масиву, який містить більше 5 - ти символів.
+
+// const artists = [
+//   "Tyler, The Creator",
+//   "Eazy-E",
+//   "Kendrick Lamar",
+//   "Kanye West",
+//   "Eminem",
+// ];
+
+// for (let i = 0; i < artists.length; i += 1) {
+//   if (artists[i].length >= 5) {
+//     console.log(`He is one of my fav artists: ${artists[i]}`);
+//   } else {
+//     console.log(`I don't have aby other fav artists tho`);
+//   }
+// }
+
+// Створити масив з 10-ти чисел.
+// Знайти та вивести на екран максимальне значення з масиву.
+
+// const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 9, 8];
+
+// let max = numbers[0];
+
+// for (let i = 0; i < numbers.length; i += 1) {
+//   if (numbers[i] > max) {
+//     max = numbers[i];
+//   }
+// }
+
+// console.log(max);
+
+// Створити масив з 10-ти чисел.
+// Знайти всі парні числа в масиві та вивести їх на екран.
+
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// for (let i = 0; i < numbers.length; i += 1) {
+//   if (numbers[i] % 2 === 0) {
+//     console.log(numbers[i]);
+//   }
+// }
+
+// AND I DIIIIIID THAAAAAAT
+// P.S. (MY FAV HW EVER)
