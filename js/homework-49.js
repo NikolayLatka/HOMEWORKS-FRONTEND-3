a6e757db2b784e694cded1edc473dbb780bfdc9c
NikolayLatka <latkaa4@gmail.com>
2025-03-11 23:05:08 +0100

    add-hw

--- js/homework-49.js
+++ js/homework-49.js
@@ -1,67 +1,61 @@
-// /*1 - Створіть функцію processArray(array, callback), яка приймає масив та функцію-колбек. Викличте processArray з різними масивами та функціями-колбеками, щоб виконати наступні операції:
-// Порахувати суму всіх елементів масиву.
-// Знайти мінімальний елемент масиву.
-// Знайти максимальний елемент масиву.
-// !!! Перепишіть колбек-функції на стрілки */
-// const processArray = (array, callback) => {};
+/*1 - Створіть функцію processArray(array, callback), яка приймає масив та функцію-колбек. 
+Викличте processArray з різними масивами та функціями-колбеками, щоб виконати наступні операції:
+Порахувати суму всіх елементів масиву.
+Знайти мінімальний елемент масиву.
+Знайти максимальний елемент масиву.
+!!! Перепишіть колбек-функції на стрілки */
+const processArray = (array, callback) => callback(array);
 
-// const getSum = function(arr) {return arr.reduce((acc, curr) => acc + curr)}
-// const getMin = function(arr) {return Math.min(...arr)}
-// const getMax = function (arr) { return Math.max(...arr) }
+const getSum = (arr) => arr.reduce((acc, curr) => acc + curr);
+const getMin = (arr) => Math.min(...arr);
+const getMax = (arr) => Math.max(...arr);
 
-// *1 - Створіть функцію processArray(array, callback), яка приймає масив та функцію-колбек.
-// Викличте processArray з різними масивами та функціями-колбеками, щоб виконати наступні операції:
-// Порахувати суму всіх елементів масиву.
-// Знайти мінімальний елемент масиву.
-// Знайти максимальний елемент масиву.
-// !!! Перепишіть колбек-функції на стрілки */
-// const processArray = (array, callback) => {
-//   return callback(array);
-// };
+const numbers = [12, 34, 56, 78, 90];
 
-// const getSum = (arr) => {
-//   return arr.reduce((acc, curr) => acc + curr);
-// };
-// const ge
-// const getMin = (arr) => {
-//   return Math.min(...arr);
-// };
-// const getMax = (arr) => {
-//   return Math.max(...arr);
-// };
-// // console.log(
-// //   processArray([1, 2, 3], getSum),
-// //   processArray([1, 2, 3], getMin),
-// //   processArray([1, 2, 3], getMax)
-// // );
+// console.log(processArray(numbers, getSum));
+// console.log(processArray(numbers, getMin));
+// console.log(processArray(numbers, getMax));
 
-// /* 2. Створіть функцію operate(a, b, callback), яка приймає два числа та колбек для виконання операції над ними. Використайте її для виконання наступних операцій:
-// Додавання.
-// Віднімання.
-// Множення.
-// Ділення.
-// !!!!  Перепишіть колбеки на стрілкові функції */
+/* 2. Створіть функцію operate(a, b, callback), 
+яка приймає два числа та колбек для виконання операції над ними. 
+Використайте її для виконання наступних операцій:
+Додавання.
+Віднімання.
+Множення.
+Ділення.
+!!!!  Перепишіть колбеки на стрілкові функції */
 
-// const operate = (a, b, callback) => {};
+const operate = (a, b, callback) => callback(a, b);
 
-// // Виклик функції з різними операціями
-// console.log(operate(10, 5, function(a, b){ return a - b})); // Віднімання
-// console.log(operate(10, 5, function(a, b){ return a + b})); // Додавання
-// console.log(operate(10, 5, function(a, b){ return a * b})); // Множення
-// console.log(operate(10, 5, function(a, b){ return a / b})); // Ділення
+const add = (a, b) => a + b;
+const substract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
 
-// // додатково
-// /*4 з ЛМС - Напишіть функцію, яка приймає масив чисел і колбек-функцію. Функція повинна застосовувати колбек-функцію до кожного елементу масиву та повертати новий масив, що містить результати застосування колбек-функції до кожного елементу. */
-// const applyCallbackToEachElement = (arr, callback) => { }
-//   const arr = [1, 2, 3, 4, 5];
-//   const squareCallback = ;
+// Виклик функції з різними операціями
+// console.log(operate(10, 5, add));
+// console.log(operate(10, 5, substract));
+// console.log(operate(10, 5, multiply));
+// console.log(operate(10, 5, divide));
 
-//   const result = applyCallbackToEachElement(arr, squareCallback);
-//   console.log(result); // [1, 4, 9, 16, 25]
-// /*5 з ЛМС - Розрахунок дисконтної ціни
-// Створіть функцію, яка розраховує дисконтну ціну на товар з урахуванням знижки. Функція повинна приймати вартість товару та відсоток знижки як аргументи. Використайте стрілкову функцію та колбек.*/
+// додатково
+/*4 з ЛМС - Напишіть функцію, яка приймає масив чисел і колбек-функцію. 
+Функція повинна застосовувати колбек-функцію до кожного елементу масиву та повертати новий масив, 
+що містить результати застосування колбек-функції до кожного елементу. */
+const applyCallbackToEachElement = (arr, callback) => arr.map(callback);
+const arr = [1, 2, 3, 4, 5];
+const squareCallback = (num) => num * num;
 
-// const calculateDiscountedPrice = (price, discount, callback) => {};
-//   const showDiscountedPrice = ;
+const result = applyCallbackToEachElement(arr, squareCallback);
+// console.log(result); // [1, 4, 9, 16, 25]
+/*5 з ЛМС - Розрахунок дисконтної ціни
+Створіть функцію, яка розраховує дисконтну ціну на товар з урахуванням знижки. 
+Функція повинна приймати вартість товару та відсоток знижки як аргументи. 
+Використайте стрілкову функцію та колбек.*/
 
-//   calculateDiscountedPrice(100, 10, showDiscountedPrice); // Discounted price: 90
+const calculateDiscountedPrice = (price, discount, callback) =>
+  callback(price - discount);
+const showDiscountedPrice = (discountedPrice) =>
+  console.log(`Discounted price: ${discountedPrice}`);
+
+calculateDiscountedPrice(100, 10, showDiscountedPrice); // Discounted price: 90
